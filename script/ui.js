addc3e955cfb199177c04ed3ad48ae3510870f19
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-24 18:47:00 +0100

    Start ui

--- script/ui.js
+++ script/ui.js
@@ -1,29 +1,13 @@
-var canvas = document.getElementById("main_canvas");
-var canvasWidth = canvas.width;
-var canvasHeight = canvas.height;
-var context = canvas.getContext("2d");
-var canvasData = context.getImageData(0, 0, canvasWidth, canvasHeight);
-
-function put_pixel(x, y, r, g, b, a)
+function show_hide(elem)
 {
-	var index = ((x + (y * canvasWidth)) * 4);
-
-	canvasData.data[index + 0] = r;
-	canvasData.data[index + 1] = g;
-	canvasData.data[index + 2] = b;
-	canvasData.data[index + 3] = a;
+	if (elem.innerHTML == "-")
+	{
+		elem.innerHTML = "+";
+		elem.parentNode.parentNode.style.height = "32px";
+	}
+	else if (elem.innerHTML == "+")
+	{
+		elem.innerHTML = "-";
+		elem.parentNode.parentNode.style.height = "auto";
+	}
 }
-
-function update_canvas()
-{
-	context.putImageData(canvasData, 0, 0);
-}
-
-put_pixel(5, 5, 255, 0, 0, 255);
-put_pixel(5, 6, 255, 0, 0, 255);
-put_pixel(5, 7, 255, 0, 0, 255);
-update_canvas();
-
-var img    = canvas.toDataURL("image/png");
-document.write('<img src="'+img+'"/>');
-window.open("file:///C:/Users/Kamipown/Desktop/Tile2d/index.html", "Window Title", "width=500, height=450");
