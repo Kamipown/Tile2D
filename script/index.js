3e08ecc8d8dc072eb5c447791ba5ddbbdb4a37a1
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-25 16:04:55 +0100

    Handle rectangular tiles

--- script/index.js
+++ script/index.js
@@ -1,23 +1,27 @@
 var background_canvas = document.getElementById("background_canvas");
 var context = background_canvas.getContext('2d');
 
-var size_span = document.getElementById("size_span");
+var width_span = document.getElementById("width_span");
+var height_span = document.getElementById("height_span");
+
+var width_input = document.getElementById("width_input");
+var height_input = document.getElementById("height_input");
 
 function set_canvas_size()
 {
-	var w = window.innerWidth;
-	var h = window.innerHeight;
-	background_canvas.width = w;
-	background_canvas.height = h;
-	draw_canvas_grid(w, h, parseInt(size_span.innerHTML));
+	background_canvas.width = window.innerWidth;
+	background_canvas.height = window.innerHeight;
+	draw_canvas_grid(parseInt(width_span.innerHTML), parseInt(height_span.innerHTML));
 }
 
-function draw_canvas_grid(w, h, val)
+function draw_canvas_grid(step_x, step_y)
 {
-	context.clearRect(0, 0, background_canvas.width, background_canvas.height)
-	var x = val;
-	var y = val;
-	context.strokeStyle = '#dddddd';
+	var x = step_x;
+	var y = step_y;
+	var w = background_canvas.width;
+	var h = background_canvas.height;
+	context.clearRect(0, 0, w, h);
+	context.strokeStyle = "#dddddd";
 	context.lineWidth = 1;
 	while (x < w)
 	{
@@ -25,7 +29,7 @@ function draw_canvas_grid(w, h, val)
 		context.moveTo(x, 0);
 		context.lineTo(x, h);
 		context.stroke();
-		x += val;
+		x += step_x;
 	}
 	while (y < h)
 	{
@@ -33,18 +37,28 @@ function draw_canvas_grid(w, h, val)
 		context.moveTo(0, y);
 		context.lineTo(w, y);
 		context.stroke();
-		y += val;
+		y += step_y;
 	}
 }
 
-function update_slide(slider)
+function update_size()
 {
-	var val = slider.value
-	size_span.innerHTML = val;
-	draw_canvas_grid(background_canvas.width, background_canvas.height, parseInt(val));
+	var size = width_input.value
+	height_input.value = size;
+	width_span.innerHTML = size;
+	height_span.innerHTML = size;
+	draw_canvas_grid(parseInt(width_span.innerHTML), parseInt(height_span.innerHTML));
+}
+
+function update_height()
+{
+	var size = height_input.value
+	height_span.innerHTML = size;
+	draw_canvas_grid(parseInt(width_span.innerHTML), parseInt(height_span.innerHTML));
 }
 
 function save_size()
 {
-	localStorage.setItem("tile_size", size_span.innerHTML);
+	localStorage.setItem("tile_width", width_span.innerHTML);
+	localStorage.setItem("tile_height", height_span.innerHTML);
 }
